42b1548bc5cc5143a9fce0643b9538d7e130d98d
Sh2-103 <Sh2-103@outlook.com>
2026-05-14 01:43:41 +0800

    style: 格式化代码

--- src/utils/session.mjs
+++ src/utils/session.mjs
@@ -1,45 +1,45 @@
-import { URL } from 'node:url';
+import { URL } from 'node:url'
 
 class Cookie {
     constructor() {
-        this._cookieMaps = new Map();
+        this._cookieMaps = new Map()
         this._cookieMaps.set('default', new Map())
     }
 
     setCookie(cookie, identifier = 'default') {
         if (!this._cookieMaps.has(identifier)) {
-            this._cookieMaps.set(identifier, new Map());
+            this._cookieMaps.set(identifier, new Map())
         }
-        const cookieMap = this._cookieMaps.get(identifier);
-        const parsedCookie = cookie.match(/^([^=]+)=([^;]*)/);
+        const cookieMap = this._cookieMaps.get(identifier)
+        const parsedCookie = cookie.match(/^([^=]+)=([^;]*)/)
 
         if (!parsedCookie) throw new Error('Cookie cannot be parsed.')
         cookieMap.set(parsedCookie[1], parsedCookie[2])
     }
 
     getCookieValue(cookieName, identifier = 'default') {
-        const cookieMap = this._getCookieMap(identifier);
-        return cookieMap.get(cookieName);
+        const cookieMap = this._getCookieMap(identifier)
+        return cookieMap.get(cookieName)
     }
 
     getHeaderString(identifier = 'default') {
-        const cookieMap = this._getCookieMap(identifier);
-        let cookies = [];
+        const cookieMap = this._getCookieMap(identifier)
+        let cookies = []
         for (const [cookieName, cookieValue] of cookieMap) {
             cookies.push(`${cookieName}=${cookieValue}`)
         }
-        return cookies.join(';');
+        return cookies.join(';')
     }
 
     getObject(identifier = 'default') {
-        const cookieMap = this._getCookieMap(identifier);
-        return Object.fromEntries(cookieMap.entries());
+        const cookieMap = this._getCookieMap(identifier)
+        return Object.fromEntries(cookieMap.entries())
     }
 
     _getCookieMap(identifier) {
-        const cookieMap = this._cookieMaps.get(identifier) || new Map();
+        const cookieMap = this._cookieMaps.get(identifier) || new Map()
         // if (!cookieMap) throw new Error('Cookie not found.');
-        return cookieMap;
+        return cookieMap
     }
 }
 
@@ -54,7 +54,9 @@ export class Session {
      * @param {*} cookieID - Cookie Identifier
      */
     setCookies(cookies, cookieID = this.cookieID) {
-        cookies.split(';').forEach(cookie => {this.cookieJar.setCookie(cookie, cookieID)})
+        cookies.split(';').forEach((cookie) => {
+            this.cookieJar.setCookie(cookie, cookieID)
+        })
     }
 
     /**
@@ -64,22 +66,23 @@ export class Session {
     getCookies(cookieID = this.cookieID) {
         return this.cookieJar.getHeaderString(cookieID)
     }
-    
+
     getCookieJar() {
-        return this.cookieJar;
+        return this.cookieJar
     }
 
-    async fetch(url, { init = {}, cookieID = this.cookieID } = {}) {
-        const cookies = this.cookieJar.getHeaderString(cookieID);
-        const headers = {...(init?.headers || {}), cookie: cookies};
+    async fetch(url, requestInit = {}, cookieID = this.cookieID) {
+        const cookies = this.cookieJar.getHeaderString(cookieID)
+        const headers = { ...(requestInit?.headers || {}), cookie: cookies }
         url = new URL(url)
 
-        const response = await fetch(url, {...init, headers: headers, redirect: 'manual'});  // `init`相当于`fetch()`的同名参数。
+        const response = await fetch(url, { ...requestInit, headers: headers, redirect: 'manual' })
 
-        const setCookieHeader = response.headers.getSetCookie();
-        if (setCookieHeader) setCookieHeader.forEach(cookie => {
-            this.cookieJar.setCookie(cookie, cookieID)
-        })
+        const setCookieHeader = response.headers.getSetCookie()
+        if (setCookieHeader)
+            setCookieHeader.forEach((cookie) => {
+                this.cookieJar.setCookie(cookie, cookieID)
+            })
         // const setCookieHeader = response.headers.get('set-cookie');
         // if (setCookieHeader) setCookieHeader.split(',').forEach(cookie => {
         //     this.cookieJar.setCookie(cookie, cookieID)
@@ -87,12 +90,12 @@ export class Session {
 
         // 处理重定向
         if (/^(3\d\d|201)$/.test(response.status)) {
-            const location = response.headers.get('location');
+            const location = response.headers.get('location')
             if (location) {
                 const newUrl = new URL(location, url.origin)
-                return await this.fetch(newUrl, {...init});
+                return await this.fetch(newUrl, { ...requestInit })
             }
         }
-        return response;
+        return response
     }
 }
